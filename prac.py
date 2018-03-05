a2218ebc6e6444cd693ceee05069db5e2512960f
Marc Bové <in.smx.82@gmail.com>
2018-03-05 14:58:25 +0100

    Initial Commit

---
+++ prac.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Tkinter import *
+from tkFileDialog import *
+import tkMessageBox 
+import os
+
+#Create window
+window=Tk()
+window.title("Cerca fitxers Redundants")
+window.minsize(500,300)
+
+#Variables
+dir_NameDst = StringVar()
+dir_NameSrc = StringVar()
+
+#Function source preguntar Banús
+def dirNameSrc():
+	dir_NameSrc.set(os.path.abspath(askdirectory()))
+
+#Function dest
+def dirNameDst():
+	dir_NameDst.set(os.path.abspath(askdirectory()))
+
+#Function 	
+def 
+
+
+#GUI's First Line: ask origin directory
+
+fDirectFont = Frame(window)
+bDirectFont = Button(fDirectFont, text = 'Escolliu directori font', command = dirNameSrc)
+lDirectFont = Label(fDirectFont, textvariable = dir_NameSrc, relief = "sunken")
+
+bDirectFont.pack(side = LEFT)
+lDirectFont.pack(side = LEFT, expand = TRUE, fill = X)
+
+
+#GUI's Second Line: ask destination directory and search
+fDirectDest = Frame(window)
+bDirectDest = Button(fDirectDest, text = 'Escolliu directori destí', command = dirNameDst)
+lDirectDest = Label(fDirectDest, textvariable = dir_NameDst, relief = "sunken")
+bCerca = Button(fDirectDest, text = 'Cerca', command = askdirectory)
+
+bDirectDest.pack(side = LEFT)
+lDirectDest.pack(side = LEFT, expand = TRUE, fill = X)
+bCerca.pack(side = LEFT)
+
+#GUI's last line: exit button
+fSortir = Frame(window)
+bSortir = Button(fSortir, text = 'Sortir', command = window.quit)
+
+bSortir.pack(side = LEFT)
+
+#GUI's second-to-last: selecciona tots/cap
+fSelecciona = Frame(window)
+bTots = Button(fSelecciona, text = 'Selecciona Tots', command = window.quit)
+bCap = Button(fSelecciona, text = 'Selecciona Cap', command = window.quit)
+
+bTots.pack(side = LEFT)
+bCap.pack(side = LEFT)
+
+#Frame for GUI's scrollboxes
+fFitxers = Frame(window)
+
+#GUI's frame for Originals scrollbox
+fOriginals = Frame(fFitxers)
+lOriginals = Label(fOriginals, text = 'Fitxers Originals:')
+scrolOriginal = Scrollbar(fOriginals, orient = VERTICAL)
+list = Listbox(fOriginals, yscrollcommand = scrolOriginal.set)
+scrolOriginal.config(command = list.yview)
+
+lOriginals.pack(side = TOP, anchor = W)
+scrolOriginal.pack(side = RIGHT, fill = Y)
+list.pack(side = LEFT, expand = TRUE, fill = BOTH)
+
+#GUI's frame for Iguals i Semblants
+fIgualSembl = Frame(fFitxers)
+
+#GUI's frame for Iguals
+fIguals = Frame(fIgualSembl)
+lIguals = Label(fIguals, text = '    Fitxers Iguals:')
+
+lIguals.pack(side = TOP, anchor = W)
+
+#Iguals' Scrollbox
+fIgualScroll = Frame(fIguals)
+scrollIguals = Scrollbar(fIgualScroll, orient = VERTICAL)
+list = Listbox(fIgualScroll, yscrollcommand = scrollIguals.set)
+scrollIguals.config(command = list.yview)
+
+scrollIguals.pack(side = LEFT, fill = Y)
+list.pack(side = RIGHT, expand = TRUE, fill = X)
+
+#Iguals' Buttons
+fFitxIgualButton = Frame(fIguals)
+bEsborra = Button(fFitxIgualButton, text = 'Esborra', command = window.quit)
+bHLink = Button(fFitxIgualButton, text = 'Hard Link', command = window.quit)
+bSLink = Button(fFitxIgualButton, text = 'Soft Link', command = window.quit)
+bSelecTotsA = Button(fFitxIgualButton, text = 'Selec Tots', command = window.quit)
+bSelecCapA = Button(fFitxIgualButton, text = 'Selec Cap', command = window.quit)
+
+bEsborra.pack(side = TOP, anchor = W)				
+bHLink.pack(side = TOP, anchor = W)				
+bSLink.pack(side = TOP, anchor = W)				
+bSelecTotsA.pack(side = TOP, anchor = W)				
+bSelecCapA.pack(side = TOP, anchor = W)
+fIgualScroll.pack(side = LEFT)
+fFitxIgualButton.pack(side = LEFT)
+fIguals.pack(side = TOP, expand = TRUE, fill = X)
+
+#GUI's frame for Semblants
+fSembl = Frame(fIgualSembl)
+lSembl = Label(fSembl, text = '    Fitxers Semblants')
+
+lSembl.pack(side = TOP, anchor = W)
+
+#Semblants' ScrollBox
+fSemblScroll = Frame(fSembl)
+scrollSembl = Scrollbar(fSemblScroll, orient = VERTICAL)
+list = Listbox(fSemblScroll, yscrollcommand = scrollSembl.set)
+scrollSembl.config(command = list.yview)
+
+scrollSembl.pack(side = LEFT, fill = Y)
+list.pack(side = RIGHT, expand = TRUE, fill = X)
+			
+#GUI's buttons for 'Fitxers Semblants'
+fFitxSemblButton = Frame(fSembl)
+bCompara = Button(fFitxSemblButton, text = 'Compara', command = window.quit)
+bRenombra = Button(fFitxSemblButton, text = 'Renombra', command = window.quit)
+bEsborra = Button(fFitxSemblButton, text = 'Esborra', command = window.quit)
+bSelecTotsB = Button(fFitxSemblButton, text = 'Selec Tots', command = window.quit)
+bSelecCapB = Button(fFitxSemblButton, text = 'Selec Cap', command = window.quit)
+
+bCompara.pack(side = TOP, anchor = W)
+bRenombra.pack(side = TOP, anchor = W)
+bEsborra.pack(side = TOP, anchor = W)
+bSelecTotsB.pack(side = TOP, anchor = W)
+bSelecCapB.pack(side = TOP, anchor = W)
+
+fSemblScroll.pack(side = LEFT)
+fFitxSemblButton.pack(side = LEFT)
+fSembl.pack(side = TOP, expand = TRUE, fill = X)
+
+#Pack frames
+fDirectFont.pack(side = TOP, expand = TRUE, fill = X)
+fDirectDest.pack(side = TOP, expand = TRUE, fill = X)
+fSortir.pack(side = BOTTOM, expand = TRUE, fill = X)
+fSelecciona.pack(side = BOTTOM, expand = TRUE, fill = X)
+fOriginals.pack(side = LEFT, expand = TRUE, fill = BOTH)
+fIgualSembl.pack(side = LEFT, expand = TRUE, fill = X)
+fFitxers.pack(side = LEFT, expand = TRUE, fill = X)
+
+window.mainloop()
