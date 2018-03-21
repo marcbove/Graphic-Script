d7001f946985d0bc6dc9451a867551d8587440b0
Marc Bove <in.smx.82@gmail.com>
2018-03-21 11:06:16 +0100

    Update
    Co-authored-by: Gwen <gwmeba@gmail.com>

---
+++ destruccio_fitxers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo -e "\nFitxers abans d'eliminar:"
+ls
+#-r recursivo, -f para que no le importe si no existe
+rm -r -f Carp1
+rm -r -f Carp2 
+rm -r -f Carp3
+rm -f ipsum.txt
+rm -f lorem.txt
+echo -e "\nDirectoris esborrats!\n\nFitxers i directoris:"
+ls
+echo ""
+exit 0
